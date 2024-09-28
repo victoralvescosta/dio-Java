1fa47ec524b1b478ac66a419567ce468c8135e4c
João Victor Alves <jvictoralvescosta@outlook.com>
2024-09-28 18:11:58 -0300

    Finalizado

---
+++ Estrutura-POO/Exercicio04/src/Calculadora.java
@@ -0,0 +1,6 @@
+public class Calculadora {
+    // Método estático que realiza a soma de dois números
+   public static int soma (int a, int b) {
+       return a + b;
+   }
+}
---
+++ Estrutura-POO/Exercicio04/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        int resultado = Calculadora.soma(10,100);
+        System.out.println("Imprimindo resultado: " + resultado);
+    }
+}
